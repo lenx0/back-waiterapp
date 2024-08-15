3c518e82eed50aa64986e280fcc3d0c44afcf1f4
thiago <thiagofreitas1106@gmail.com>
2024-08-15 10:33:34 -0300

    router, useCases, uploads

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
+
+import path from 'node:path';
 import express from "express";
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017')
+import { router } from './router'
+
+mongoose.connect('mongodb+srv://waiterappmongo:q6UoTxWxPYahY2JD@cluster0.ybga9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => {
   const app = express();
   const port = 3001;
 
+  app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+  app.use(express.json());
+  app.use(router);
+
 app.listen(port, () => {
+  console.log("MongoDB is connected")
   console.log(`Server is running on port ${port}`);
 });
 })
