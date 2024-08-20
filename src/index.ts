b3628167ee6477765fe96db8eb1a21b6f38ecc5d
thiago <thiagofreitas1106@gmail.com>
2024-08-20 10:09:11 -0300

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,12 @@ mongoose.connect('mongodb+srv://waiterappmongo:q6UoTxWxPYahY2JD@cluster0.ybga9.m
   const app = express();
   const port = 3001;
 
-  app.use((req, res) => {
-    res.setHeader('Acess-Control-Allow-Origin', '*')
-    res.setHeader('Acess-Control-Allow-Methods', '*')
-    res.setHeader('Acess-Control-Allow-Headers', '*')
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', '*')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+
+    next();
   })
 
   app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
